b9e886b3bca7c703a874f83997c7353cd4e6e248
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-04-28 18:54:50 -0800

    Identifies qr closest to target and adds color

--- Core/qrToHuman.py
+++ Core/qrToHuman.py
@@ -2,6 +2,11 @@ import cv2
 import pyzbar.pyzbar as pyzbar
 # import argparse
 import imutils
+import math
+
+closestQR = {'distance': 0, 'data': ''}
+lineColor = (255, 0, 0)
+closestColor = (0, 255, 0)
 
 
 def parseImage(image):
@@ -12,22 +17,24 @@ def parseImage(image):
     return objects
 
 
-def showQRBox(image, object_data):
-    lineColor = (255, 0, 0)
+def showBoxes(image, object_data):
     for object in object_data:
         qrPoints = len(object.location)
         midPoint = (0, 0)
         for i in range(0, qrPoints):
-            print(object.location[i])
-            midPoint += (midPoint[0] + object.location[i][0], midPoint[1] + object.location[i][1])
-            print("MID: ", midPoint)
-            # midPoint[1] += object.location[i][1]
+            midPoint = (midPoint[0] + object.location[i][0], midPoint[1]
+                        + object.location[i][1])
             cv2.line(image, object.location[i], object.location[(i+1)
                      % qrPoints], lineColor, 3)
         midPoint = (int(midPoint[0] / qrPoints), int(midPoint[1] / qrPoints))
-        # midPoint[1] /= qrPoints
-        # print(midPoint)
-        cv2.circle(image, midPoint, 3, lineColor)
+        # TODO: INTEGRATE MOTION DETECT
+        humanLocation = (0, 0, 0)
+        absDiff = math.sqrt(abs((midPoint[0] - humanLocation[0])
+                            + (midPoint[1] - humanLocation[1])))
+        if closestQR['data'] == '' or absDiff < closestQR['distance']:
+            closestQR['distance'] = absDiff
+            closestQR['data'] = object.data
+            cv2.circle(image, midPoint, 10, closestColor, -1)
     cv2.imshow("QR: ", image)
     cv2.waitKey(0)
 
@@ -35,6 +42,6 @@ def showQRBox(image, object_data):
 if __name__ == '__main__':
     qr_source = cv2.imread("qr_test/above1.jpg")
     qr_source = imutils.resize(qr_source, width=500)
-    gray = cv2.cvtColor(qr_source, cv2.COLOR_BGR2GRAY)
-    objects = parseImage(gray)
-    showQRBox(gray, objects)
+    # gray = cv2.cvtColor(qr_source, cv2.COLOR_BGR2GRAY)
+    objects = parseImage(qr_source)
+    showBoxes(qr_source, objects)
