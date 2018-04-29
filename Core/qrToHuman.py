4e26f15b35f036a3179f5ba9c8889a1e509cc123
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-04-28 20:01:13 -0800

    Gets Z distance from qr code to camera

--- Core/qrToHuman.py
+++ Core/qrToHuman.py
@@ -1,3 +1,9 @@
+# qrToHuman.py
+# Gets the distance/device info of the closest QR code and human
+# Test Device: Google Pixel 1
+#   Focal Length: 26mm
+#   Sensor Height: 7.81mm
+
 import cv2
 import pyzbar.pyzbar as pyzbar
 # import argparse
@@ -7,6 +13,19 @@ import math
 closestQR = {'distance': 0, 'data': ''}
 lineColor = (255, 0, 0)
 closestColor = (0, 255, 0)
+
+# 5 inches
+actualQRHeight = 127
+
+focalLength = 4.67
+sensorHeight = 7.81
+
+
+def distance(obj1, obj2):
+    absDistance = 0
+    for dim in range(0, len(obj1)):
+        absDistance += (obj1[dim] - obj2[dim]) ** 2
+    return math.sqrt(absDistance)
 
 
 def parseImage(image):
@@ -23,14 +42,25 @@ def showBoxes(image, object_data):
         midPoint = (0, 0)
         for i in range(0, qrPoints):
             midPoint = (midPoint[0] + object.location[i][0], midPoint[1]
-                        + object.location[i][1])
-            cv2.line(image, object.location[i], object.location[(i+1)
+                      + object.location[i][1])
+            cv2.line(image, object.location[i], object.location[(i + 1)
                      % qrPoints], lineColor, 3)
         midPoint = (int(midPoint[0] / qrPoints), int(midPoint[1] / qrPoints))
+
+        sideLength = 0
+        for i in range(1, qrPoints):
+            sideLength += distance(object.location[i-1], object.location[i])
+        sideLength /= qrPoints
+
+        h, w, c = image.shape
+        print("IMG HEIGHT: ", h)
+        print("SIDE LENGTH: ", sideLength)
+        zDistance = (focalLength * actualQRHeight * h) / (sideLength * sensorHeight)
+        print("Z-DISTANCE: ", zDistance)
+        # midPoint += zDistance
         # TODO: INTEGRATE MOTION DETECT
         humanLocation = (0, 0, 0)
-        absDiff = math.sqrt(abs((midPoint[0] - humanLocation[0])
-                            + (midPoint[1] - humanLocation[1])))
+        absDiff = distance(midPoint, humanLocation)
         if closestQR['data'] == '' or absDiff < closestQR['distance']:
             closestQR['distance'] = absDiff
             closestQR['data'] = object.data
