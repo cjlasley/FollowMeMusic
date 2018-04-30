6cccf40806225148448b9d202cc63df8d3a2d43b
Thatcher Lane <thatcherlane@Thatchers-MacBook-Pro.local>
2018-04-29 16:24:34 -0800

    making cascade detection into a function so it can be imported

--- Core/detect_person.py
+++ Core/detect_person.py
@@ -8,21 +8,22 @@ body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 cap = cv2.VideoCapture(0)
 image = cv2.imread('testPeople.jpeg', cv2.IMREAD_GRAYSCALE);
 
-while 1:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    body = body_cascade.detectMultiScale(gray, 1.1, 3)
 
-    for (x,y,w,h) in body:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+def detectBody():
+    while 1:
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        body = body_cascade.detectMultiScale(gray, 1.1, 3)
 
-    cv2.imshow('img',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+        for (x,y,w,h) in body:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
 
+        cv2.imshow('img',img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
