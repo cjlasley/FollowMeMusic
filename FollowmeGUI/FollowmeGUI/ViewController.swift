48a8e98c086c43a46b87f43ae3156701bc1548eb
Brandon Abbott <barabbit907@gmail.com>
2018-05-01 20:48:37 -0800

    added gui and configuration for server

---
+++ FollowmeGUI/FollowmeGUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FollowmeGUI
+//
+//  Created by Brandon Abbott on 4/29/18.
+//  Copyright © 2018 Brandon Abbott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
